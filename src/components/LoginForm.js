d78ba50d90648813568c680b3c8bcce41175c401
AJ Genung <DragonRydrz@me.com>
2018-04-14 10:07:07 -0400

    sign in/create failed built

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import { Text } from 'react-native';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
+    error: '',
   };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then()
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then()
+          .catch(() => {
+            this.setState({ error: 'Authentication Failed' });
+          });
+      });
+  }
+
   render() {
     return (
       <Card>
@@ -26,12 +50,23 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
         <CardSection>
-          <Button>Log in</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
 
 export default LoginForm;
